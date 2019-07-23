d77ba2d257beab67cffca483194d9b532b6616f8
aman0000 <39732091+aman0000@users.noreply.github.com>
2019-07-23 20:32:27 +0530

    Last Commit

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -2,9 +2,7 @@ import React from 'react';
 import './NavigationItem.css'
 const navigationItem = (props) => (
     <li className="NavigationItem">
-        <a
-            href={props.link}
-            className={props.active ? 'active' : null}>{props.children}</a>
+        <a href={props.link} className={props.active ? 'active' : null}>{props.children}</a>
     </li>
 );
 
--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,7 +1,11 @@
-import React from 'react';
-
-const drawerToggle=(props)=>(
-    <div onClick={props.clicked}>Menu</div>
-)
+import React from "react";
+import './DrawerToggle.css'
+const drawerToggle = props => (
+  <div className="DrawerToggle" onClick={props.clicked}>
+    <div />
+    <div />
+    <div />
+  </div>
+);
 
 export default drawerToggle;
